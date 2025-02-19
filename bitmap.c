64c2da0e49eb7ce7ed7604becb890946d270e9f4
Bhargavi <74898002+kubhargavi@users.noreply.github.com>
2025-02-19 16:16:28 -0500

    Update bitmap.c

--- bitmap.c
+++ bitmap.c
@@ -1,19 +1,5 @@
-/*
- * CS 551 Project "Memory manager".
- * This file needs to be turned in.	
- */
-
 #include "common.h"
 
-/*
- * Find the position (starting from 0) of the first bit that has value "val" in the "bitmap". 
- * @param bitmap: start address of the bitmap
- * @param size: size in bytes of the bitmap
- * @param val: 0 or non-0
- * @return: the position in the bitmap of the first 0 bit (if "val" is zero) or the first 1 bit (if "val" is non-zero)
- *          BITMAP_OP_NOT_FOUND (defined in "common.h") if the bit of interest does not exist in the bitmap
- *          BITMAP_OP_ERROR (defined in "common.h") on any other errors
- */
 int bitmap_find_first_bit(unsigned char * bitmap, int size, int val)
 {
 	for (int i = 0; i < size * 8; i++) {
@@ -25,14 +11,6 @@ int bitmap_find_first_bit(unsigned char * bitmap, int size, int val)
     return BITMAP_OP_NOT_FOUND;
 }
 
-/*
- * Change the value of the "target_pos"-th bit (starting from 0) in the "bitmap" to 1.
- * @param bitmap: start address of the bitmap
- * @param size: size in bytes of the bit map
- * @param target_pos: the position of the bit of interest
- * @return: BITMAP_OP_SUCCEED (defined in "common.h") on success
- *          BITMAP_OP_ERROR (defined in "common.h") on any errors
- */
 int bitmap_set_bit(unsigned char * bitmap, int size, int target_pos)
 {
     if (target_pos < 0 || target_pos >= size * 8) {
@@ -42,14 +20,6 @@ int bitmap_set_bit(unsigned char * bitmap, int size, int target_pos)
     return BITMAP_OP_SUCCEED;
 }
 
-/*
- * Change the value of the "target_pos"-th bit (starting from 0) in the "bitmap" to 0.
- * @param bitmap: start address of the bitmap
- * @param size: size in bytes of the bit map
- * @param target_pos: the position of the bit of interest
- * @return: BITMAP_OP_SUCCEED (defined in "common.h") on success
- *          BITMAP_OP_ERROR (defined in "common.h") on any errors
- */
 int bitmap_clear_bit(unsigned char * bitmap, int size, int target_pos)
 {
      if (target_pos < 0 || target_pos >= size * 8) {
@@ -59,15 +29,6 @@ int bitmap_clear_bit(unsigned char * bitmap, int size, int target_pos)
     return BITMAP_OP_SUCCEED;
 }
 
-
-/*
- * Test if the value of the "pos"-th bit (starting from 0) in the "bitmap" is 1.
- * @param bitmap: start address of the bitmap
- * @param size: size in bytes of the bit map
- * @param pos: the position of the bit of interest
- * @return: the value of the bit of interest (i.e., 0 or 1) on success
- *          BITMAP_OP_ERROR (defined in "common.h") on any errors
- */
 int bitmap_bit_is_set(unsigned char * bitmap, int size, int pos)
 {
     if (pos < 0 || pos >= size * 8) {
@@ -76,14 +37,6 @@ int bitmap_bit_is_set(unsigned char * bitmap, int size, int pos)
     return (bitmap[pos / 8] >> (pos % 8)) & 1;
 }
 
-/*
- * Print the content of a bitmap in starting from the first bit, and insert a space every 4 bits
- * @param bitmap: start address of the bitmap
- * @param size: size in bytes of the bit map
- * @return: BITMAP_OP_SUCCEED (defined in "common.h") on success
- *          BITMAP_OP_ERROR (defined in "common.h") on any errors
- * Do not change the implementation of this function.
- */
 int bitmap_print_bitmap(unsigned char * bitmap, int size)
 {
     int pos = 0;
